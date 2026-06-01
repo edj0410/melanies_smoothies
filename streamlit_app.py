ffaee35fcbe4864f3ac4bc5468d286eb1ae82b92
edj0410 <edj0410@gmail.com>
2026-06-01 14:33:47 +0100

    Update streamlit_app.py
    fix line 39 bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
        
-        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅)
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 # New section to display smoothiefroot nutrition information
 import requests
